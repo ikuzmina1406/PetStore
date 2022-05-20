8e407ffb0a1ce9a1ca8158861cdd819802deb288
ikuzmina1406 <ikuzmina1406@gmail.com>
2022-05-21 01:07:40 +0300

    Initial commit. All types of requests are implemented

---
+++ src/main/java/ua/GoIt/App.java
@@ -0,0 +1,10 @@
+package ua.GoIt;
+
+
+import ua.GoIt.console.ConsoleApp;
+
+public class App {
+    public static void main(String[] args) {
+        ConsoleApp.run();
+    }
+}
---
+++ src/main/java/ua/GoIt/command/Command.java
@@ -0,0 +1,5 @@
+package ua.GoIt.command;
+
+public interface Command {
+        void handle();
+}
